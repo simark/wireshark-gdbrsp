131736897d1d9c0e2f5261a5150a1fe0811d5ad7
Simon Marchi <simon.marchi@polymtl.ca>
2014-05-31 23:57:57 -0400

    More tvb refactor

--- packet-gdbrsp.c
+++ packet-gdbrsp.c
@@ -611,13 +611,13 @@ static struct dissect_command_t cmd_cbs[] = {
 	{ "T", dissect_cmd_T, dissect_reply_T, NULL },
 };
 
-static struct dissect_command_t *find_command(tvbuff_t *tvb, guint offset) {
+static struct dissect_command_t *find_command(tvbuff_t *tvb) {
 	struct dissect_command_t *cmd;
 
 	for (cmd = cmd_cbs; cmd->command != NULL; cmd++) {
 		int command_len = strlen(cmd->command);
 
-		if (tvb_strneql(tvb, offset, cmd->command, command_len) == 0) {
+		if (tvb_strneql(tvb, 0, cmd->command, command_len) == 0) {
 			return cmd;
 		}
 	}
@@ -625,7 +625,7 @@ static struct dissect_command_t *find_command(tvbuff_t *tvb, guint offset) {
 	return NULL;
 }
 
-static void dissect_one_host_query(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint offset, guint msg_len,
+static void dissect_one_host_query(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree,
 		struct gdbrsp_conv_data *conv) {
 	struct dissect_command_t *cmd;
 	int command_name_len = 0;
@@ -634,8 +634,7 @@ static void dissect_one_host_query(tvbuff_t *tvb, packet_info *pinfo, proto_tree
 	struct per_packet_data *packet_data = p_get_proto_data(pinfo->fd, proto_gdbrsp, 0);
 
 	if (!pinfo->fd->flags.visited) {
-		/* Skip $ */
-		packet_data->command = find_command(tvb, offset + 1);
+		packet_data->command = find_command(tvb);
 
 		conv->last_command = packet_data->command;
 		conv->last_packet_framenum = pinfo->fd->num;
@@ -654,7 +653,7 @@ static void dissect_one_host_query(tvbuff_t *tvb, packet_info *pinfo, proto_tree
 
 	/* Add command name entry to tree */
 	if (tree) {
-		proto_tree_add_string(tree, hf_command, tvb, offset + 1, command_name_len, cmd->command);
+		proto_tree_add_string(tree, hf_command, tvb, 0, command_name_len, cmd->command);
 	}
 
 	/* Set info column */
@@ -663,7 +662,7 @@ static void dissect_one_host_query(tvbuff_t *tvb, packet_info *pinfo, proto_tree
 
 	/* Call command handler, skip $ and command name */
 
-	tvbuff_t *subset_tvbuff = tvb_new_subset_length(tvb, offset + 1 + command_name_len, msg_len - 1 - command_name_len);
+	tvbuff_t *subset_tvbuff = tvb_new_subset_remaining(tvb, command_name_len);
 	cmd->command_handler(subset_tvbuff, pinfo, tree, conv);
 
 	if (tree) {
@@ -674,7 +673,7 @@ static void dissect_one_host_query(tvbuff_t *tvb, packet_info *pinfo, proto_tree
 		PROTO_ITEM_SET_URL(pi);
 	}
 }
-static void dissect_one_stub_reply(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint offset, guint msg_len,
+static void dissect_one_stub_reply(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree,
 		struct gdbrsp_conv_data *conv) {
 	struct dissect_command_t *cmd;
 	printf("Stub reply\n");
@@ -705,18 +704,16 @@ static void dissect_one_stub_reply(tvbuff_t *tvb, packet_info *pinfo, proto_tree
 	col_append_str(pinfo->cinfo, COL_INFO, " to ");
 	col_append_str(pinfo->cinfo, COL_INFO, cmd->command);
 
-	/* Skip $ */
-	tvbuff_t *subset_tvbuff = tvb_new_subset_length(tvb, offset + 1, msg_len - 1);
-	cmd->reply_handler(subset_tvbuff, pinfo, tree, conv);
+	cmd->reply_handler(tvb, pinfo, tree, conv);
 
 	if (tree) {
 		proto_tree_add_uint(tree, hf_request_in, tvb, 0, 0, packet_data->matching_framenum);
 	}
 }
 
-static void dissect_one_ack(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint offset, guint msg_len,
+static void dissect_one_ack(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree,
 		struct gdbrsp_conv_data *conv) {
-	printf("Stub ack %d\n", msg_len);
+	printf("Stub ack %d\n", tvb_length(tvb));
 
 	struct per_packet_data *packet_data = p_get_proto_data(pinfo->fd, proto_gdbrsp, 0);
 
@@ -725,11 +722,11 @@ static void dissect_one_ack(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree,
 		packet_data->matching_framenum = conv->last_packet_framenum;
 	}
 
-	guint8 c = tvb_get_guint8(tvb, offset);
-	if (msg_len == 1 && (c == '+' || c == '-')) {
+	guint8 c = tvb_get_guint8(tvb, 0);
+	if (tvb_length(tvb) == 1 && (c == '+' || c == '-')) {
 		if (tree) {
 			// Add protocol section in the packet details
-			proto_tree_add_boolean(tree, hf_ack, tvb, offset, 1, c == '+' ? TRUE : FALSE);
+			proto_tree_add_boolean(tree, hf_ack, tvb, 0, 1, c == '+' ? TRUE : FALSE);
 
 			if (packet_data->matching_framenum > 0) {
 				proto_tree_add_uint(tree, hf_ack_to, tvb, 0, 0, packet_data->matching_framenum);
@@ -738,9 +735,9 @@ static void dissect_one_ack(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree,
 	}
 }
 
-static void dissect_one_host_ack(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint offset, guint msg_len,
+static void dissect_one_host_ack(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree,
 		struct gdbrsp_conv_data *conv) {
-	printf("Host ack %d\n", msg_len);
+	printf("Host ack %d\n", tvb_length(tvb));
 
 	if (!pinfo->fd->flags.visited) {
 		if (conv->disable_ack_at_next_host_ack) {
@@ -749,17 +746,17 @@ static void dissect_one_host_ack(tvbuff_t *tvb, packet_info *pinfo, proto_tree *
 		}
 	}
 
-	dissect_one_ack(tvb, pinfo, tree, offset, msg_len, conv);
+	dissect_one_ack(tvb, pinfo, tree, conv);
 	col_append_str(pinfo->cinfo, COL_INFO, "Host acknowledgement");
 }
 
-static void dissect_one_stub_ack(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint offset, guint msg_len,
+static void dissect_one_stub_ack(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree,
 		struct gdbrsp_conv_data *conv) {
-	dissect_one_ack(tvb, pinfo, tree, offset, msg_len, conv);
+	dissect_one_ack(tvb, pinfo, tree, conv);
 	col_append_str(pinfo->cinfo, COL_INFO, "Stub acknowledgement");
 }
 
-static void dissect_one_notification(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint offset, guint msg_len,
+static void dissect_one_notification(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree,
 		struct gdbrsp_conv_data *conv) {
 	col_append_str(pinfo->cinfo, COL_INFO, "Notification");
 	printf("Notification\n");
@@ -781,8 +778,8 @@ static struct gdbrsp_conv_data *get_conv_data(packet_info *pinfo) {
 
 	return conv_data;
 }
-
-static void dissect_crc(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint offset, guint msg_len,
+/*
+static void dissect_crc(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree,
 		struct gdbrsp_conv_data *conv) {
 	int i;
 	char packet_crc[3];
@@ -808,7 +805,7 @@ static void dissect_crc(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, gui
 		proto_tree_add_uint_format_value(tree, hf_checksum, tvb, i, 2, crc, "%s (computed crc: %s)", packet_crc,
 				computed_crc);
 	}
-}
+}*/
 
 // If the available data contains a complete message, return the length of that message. Otherwise, return 0.
 static gint find_next_message_len(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint offset) {
@@ -881,8 +878,11 @@ static enum gdb_msg_type determine_type(struct gdbrsp_conv_data *conv, guint8 fi
 	return this_type;
 }
 
-static void dissect_one_gdbrsp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint offset, guint msg_len) {
+static void dissect_one_gdbrsp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree) {
 	proto_item *ti;
+	tvbuff_t *subset_tvb = NULL;
+	//tvbuff_t *crc_tvb = NULL;
+	guint msg_len;
 
 	if (check_col(pinfo->cinfo, COL_PROTOCOL)) {
 		col_set_str(pinfo->cinfo, COL_PROTOCOL, "GDB-RSP");
@@ -894,7 +894,7 @@ static void dissect_one_gdbrsp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tr
 
 	// Add gdb subtree
 	if (tree) {
-		ti = proto_tree_add_item(tree, proto_gdbrsp, tvb, offset, msg_len, ENC_NA);
+		ti = proto_tree_add_item(tree, proto_gdbrsp, tvb, 0, tvb_length(tvb), ENC_NA);
 		tree = proto_item_add_subtree(ti, ett_gdbrsp);
 	}
 
@@ -915,22 +915,33 @@ static void dissect_one_gdbrsp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tr
 	// Dissect based on what we expect to have
 	switch (packet_data->type) {
 	case GDB_HOST_QUERY:
-		dissect_one_host_query(tvb, pinfo, tree, offset, msg_len - crc_len, conv);
-		dissect_crc(tvb, pinfo, tree, offset + 1, msg_len - 1, conv);
+		// Skip $
+		msg_len = tvb_length(tvb) - 1 - crc_len;
+		subset_tvb = tvb_new_subset_length(tvb, 1, msg_len);
+		dissect_one_host_query(subset_tvb, pinfo, tree, conv);
+
+		// Skip $ and #
+		//crc_tvb = tvb_new_subset_remaining(tvb, msg_len + 1 /* $ */ + 1 /* # */);
+		//dissect_crc(crc_tvb, pinfo, tree, conv);
 		break;
 	case GDB_HOST_ACK:
-		dissect_one_host_ack(tvb, pinfo, tree, offset, msg_len, conv);
+		dissect_one_host_ack(tvb, pinfo, tree, conv);
 		break;
 	case GDB_STUB_REPLY:
-		dissect_one_stub_reply(tvb, pinfo, tree, offset, msg_len - crc_len, conv);
-		dissect_crc(tvb, pinfo, tree, offset + 1, msg_len - 1, conv);
+		msg_len = tvb_length(tvb) - 1 - crc_len;
+		subset_tvb = tvb_new_subset_length(tvb, 1, msg_len);
+		dissect_one_stub_reply(subset_tvb, pinfo, tree, conv);
+		//dissect_crc(tvb, pinfo, tree, offset + 1, msg_len - 1, conv);
 		break;
 	case GDB_STUB_ACK:
-		dissect_one_stub_ack(tvb, pinfo, tree, offset, msg_len, conv);
+		dissect_one_stub_ack(tvb, pinfo, tree, conv);
 		break;
 	case GDB_NOTIFICATION:
-		dissect_one_notification(tvb, pinfo, tree, offset, msg_len - crc_len, conv);
-		dissect_crc(tvb, pinfo, tree, offset + 1, msg_len - 1, conv);
+		// Skip %
+		msg_len = tvb_length(tvb) - 1 - crc_len;
+		subset_tvb = tvb_new_subset_length(tvb, 1, msg_len);
+		dissect_one_notification(tvb, pinfo, tree, conv);
+		//dissect_crc(tvb, pinfo, tree, offset + 1, msg_len - 1, conv);
 		break;
 	}
 }
@@ -939,13 +950,16 @@ static void dissect_gdbrsp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree) 
 	guint offset = 0;
 	const guint max_offset = tvb_reported_length(tvb);
 	guint msg_len;
+	tvbuff_t *msg_tvb = NULL;
 
 	printf("dissect_gdbrsp with %d bytes\n", max_offset);
 
 	while (tvb_offset_exists(tvb, offset)) {
 		msg_len = find_next_message_len(tvb, pinfo, tree, offset);
+
 		if (msg_len > 0) {
-			dissect_one_gdbrsp(tvb, pinfo, tree, offset, msg_len);
+			msg_tvb = tvb_new_subset_length(tvb, offset, msg_len);
+			dissect_one_gdbrsp(msg_tvb, pinfo, tree);
 			offset += msg_len;
 		} else if (msg_len == 0) {
 			printf("Not enough data, asking for more\n");
